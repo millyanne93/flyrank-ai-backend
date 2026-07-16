e37d34d14de0a482ee56310f87499e3927c46e84
millyanne93 <nmillyanne20@gmail.com>
2026-07-16 13:23:01 +0300

    Stage 0: hello server

--- task-1-simple-api/server.ts
+++ task-1-simple-api/server.ts
@@ -3,16 +3,11 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello, world!' });
-});
 
-app.get('/ping', (req: Request, res: Response) => {
-  res.json({ status: 'ok', message: 'pong' });
+app.get('/', (req: Request, res: Response): void=> {
+  res.json({ message: 'Hello world!'});
 });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
-  console.log(`- http://localhost:${PORT}/`);
-  console.log(`- http://localhost:${PORT}/ping`);
 });
